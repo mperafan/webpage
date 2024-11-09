3b132d597654f5c2b033383f74f6faf1555e15cd
mperafan <mirandaperafan@gmail.com>
2024-11-09 10:56:11 -0300

    change icons and tittle and comment the size project

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -1,9 +1,12 @@
 import React, { createContext, useState } from "react";
 import { PRODUCTS } from "../products";
 
+
 export const ShopContext = createContext(null);
 
-const getDefaultCart = () => {
+//Project: add sizes
+
+/*const getDefaultCart = () => {
     let cart = {}
     for (let i=1;i<PRODUCTS.length +1; i++){
         cart[i] = {
@@ -18,7 +21,16 @@ const getDefaultCart = () => {
         };
     }
     return cart;
-};
+};*/
+
+const getDefaultCart = () => {
+    let cart = {};
+    for (let i = 1; i < PRODUCTS.length + 1; i++) {
+      cart[i] = 0;
+    }
+    return cart;
+  };
+  
 
 export const ShopContextProvider = (props) => {
     const [cartItems, setCartItems] = useState(getDefaultCart());
@@ -26,13 +38,26 @@ export const ShopContextProvider = (props) => {
     const getTotalCartAmount = () => {
         let totalAmount = 0;
         for (const item in cartItems) {
-            if (cartItems[item]> 0){
-                let itemInfo = PRODUCTS.find((product) => product.id== Number(item));
-                totalAmount += cartItems[item] * itemInfo.price;
-            };
+          if (cartItems[item] > 0) {
+            let itemInfo = PRODUCTS.find((product) => product.id === Number(item));
+            totalAmount += cartItems[item] * itemInfo.price;
+          }
+        }
+        return totalAmount;
+      };
+
+   /* const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            for(const itemj in item){
+                if(cartItems[item][itemj] > 0 ){
+                    let itemInfo = PRODUCTS.find((product) => product.id === Number(item));
+                    totalAmount += cartItems[item][itemj] * itemInfo.price;
+                }
+            }
         };
         return totalAmount;
-    };
+    };*/
 
     const addToCart = (itemId) => {
         setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1}));
@@ -43,6 +68,11 @@ export const ShopContextProvider = (props) => {
     const updateCartItemCount = (newAmount, itemId) => {
         setCartItems((prev) => ({...prev, [itemId]: newAmount}));
     }
-    const contextValue= {cartItems, addToCart, removeFromCart, updateCartItemCount, getTotalCartAmount};
+
+    const checkout = () => {
+        setCartItems(getDefaultCart());
+      };
+
+    const contextValue= {cartItems, addToCart, removeFromCart, updateCartItemCount, getTotalCartAmount, checkout};
     return <ShopContext.Provider value={contextValue}>{props.children}</ShopContext.Provider>;
 };
--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+'use client'
+import React, { useState } from "react";
 import { ShopContext } from "../../context/shop-context";
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 export const Product = (props) => {
     const {id, productName, price, productImage} = props.data;
     const { addToCart, cartItems}= useContext(ShopContext);
-    console.log(props)
-const cartItemAmount = cartItems[id];
+    const [collapsed, setCollapsed] = useState(false)
+    
+    const cartItemAmount = cartItems[id];
 
-    return( <div className="product">
+//Project add sizes
+
+   /* return( <div className="product">
         <Link to={`/${id}`}><img className='logo' src={productImage}/></Link>
         <div className="description">
             <p>
@@ -17,7 +21,34 @@ const cartItemAmount = cartItems[id];
             </p>
             <p> ${price}</p>
         </div>
-        <button className="addToCartBttn" onClick={()=> addToCart(id)}>Añadir al carrito{cartItemAmount > 0 && <> ({cartItemAmount})</>}</button>
+        <div className="addToCartBttn" onClick={() => setCollapsed(!collapsed)}>Añadir al carrito</div>
+        <div className="BttnCollapse">{collapsed && <div>
+                                                        <p>S</p>
+                                                        <p>M</p>
+                                                        <p>L</p>
+                                                        <p>XL</p>
+                                                        <p>2XL</p>
+                                                        <p>3XL</p>
+                                                        <p>4XL</p>
+                                                        <p>5XL</p>
+                                                    </div>}
+        </div>
     </div>
     );
+};*/
+
+  return (
+    <div className="product">
+      <img src={productImage} />
+      <div className="description">
+        <p>
+          <b>{productName}</b>
+        </p>
+        <p> ${price}</p>
+      </div>
+      <button className="addToCartBttn" onClick={() => addToCart(id)}>
+        Añadir al carrito {cartItemAmount > 0 && <> ({cartItemAmount})</>}
+      </button>
+    </div>
+  );
 };
